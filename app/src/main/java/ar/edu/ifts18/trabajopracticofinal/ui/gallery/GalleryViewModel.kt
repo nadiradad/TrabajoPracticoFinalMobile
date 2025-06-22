f2cb0b48f1e470082b223cabdbf6aae0fa940b0d
nadiradad <nadiradad@gmail.com>
2025-06-21 23:32:34 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/ifts18/trabajopracticofinal/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package ar.edu.ifts18.trabajopracticofinal.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
